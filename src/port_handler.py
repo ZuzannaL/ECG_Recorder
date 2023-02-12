75a1c02f44c32d04f343f2cf88a7f7cfef351fb5
zuzanna <zuzannalewandowska97@gmail.com>
2023-02-12 23:39:41 +0100

    port_handler.py refactor

--- src/port_handler.py
+++ src/port_handler.py
@@ -2,16 +2,19 @@ from datetime import datetime
 import serial
 from serial.tools.list_ports import comports
 
+
 def convert_units_to_volts(value, adc_resolution=12, max_voltage=3.3):
     quantization_level = 2**adc_resolution
     max_value = quantization_level-1
     return value / max_value * max_voltage
 
+
 def find_available_ports():
     return [port.name for port in comports()]
 
-def read_from_serial_port(port, baudrate, **kwargs):
-    # configure the serial connections (the parameters differs on the device you are connecting to)
+
+def read_from_serial_port(port, baudrate):
+    # device-dependent configuration of serial connection
     ser = serial.Serial(
         port=port,
         baudrate=baudrate,
@@ -35,6 +38,7 @@ def read_from_serial_port(port, baudrate, **kwargs):
                 else:
                     continue
 
+
 def write_all_data_to_file(filename, port, baudrate):
     print(f'Saving to file {filename}')
     with open(filename, 'w') as file:
@@ -44,28 +48,28 @@ def write_all_data_to_file(filename, port, baudrate):
             if out != '':
                 file.write(out)
 
+
 def write_data_point_to_file(x, file):
     out = ''
     out += str(x)+'\n'
     if out != '':
         file.write(out)
 
-
-if __name__ == '__main__':
+def main():
     from enum import auto, Enum
 
     class System(Enum):
         LINUX = auto()
         WINDOWS = auto()
 
-    baudrate = 9600 #38400
+    baudrate = 38400
     system = System.WINDOWS
 
-    if system==System.LINUX:
+    if system == System.LINUX:
         filename = f'../data/{datetime.now().strftime("%Y-%m-%d_%H%M%S")}.txt'
         port = '/dev/serial/by-id/usb-STMicroelectronics_STM32_STLink_066CFF575353898667173738-if02'
-        #port = '/dev/pts/3'
-    elif system==System.WINDOWS:
+        # port = '/dev/pts/3'
+    elif system == System.WINDOWS:
         filename = f'..\data\{datetime.now().strftime("%Y-%m-%d_%H%M%S")}.txt'
         port = 'COM7'
     else:
@@ -73,4 +77,7 @@ if __name__ == '__main__':
 
     write_all_data_to_file(filename, port, baudrate)
 
+
+if __name__ == '__main__':
+    main()
 
