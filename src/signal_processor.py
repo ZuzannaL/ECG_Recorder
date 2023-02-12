8750bf66ece9134577c000855abe471437ca3fb1
zuzanna <zuzannalewandowska97@gmail.com>
2023-02-12 17:34:53 +0100

    Signal processing code refactor, add converting units to volts in offline plots, add labels to the plots. Here there was found a problem with find_peaks_using_correlation function - it works very bad with long signals because it is very sensitive to outliers in correlation

--- src/signal_processor.py
+++ src/signal_processor.py
@@ -3,17 +3,16 @@ import matplotlib.pyplot as plt
 import scipy.signal as ss
 from enum import auto, Enum
 import heartpy as hp
+from port_handler import convert_units_to_volts
 
-plt.rcParams.update({'font.family': 'Calibri'})
-plt.rcParams.update({'font.size': 12})
-
-params = {'legend.fontsize': 12,
-          'axes.labelsize': 14,
-          'axes.titlesize': 20,
-          'xtick.labelsize': 12,
-          'ytick.labelsize': 12}
-
-plt.rcParams.update(params)
+matplotlib_plots_params = {'font.family': 'Calibri',
+                           'font.size': 12,
+                           'legend.fontsize': 12,
+                           'axes.labelsize': 14,
+                           'axes.titlesize': 20,
+                           'xtick.labelsize': 12,
+                           'ytick.labelsize': 12}
+plt.rcParams.update(matplotlib_plots_params)
 
 NUMBER_OF_SEC_IN_ONE_MIN = 60
 
@@ -92,7 +91,8 @@ class SignalProcessor:
         peaks_indices, _ = ss.find_peaks(s, height=350, distance=66)
         return peaks_indices
 
-    def find_peaks_using_correlation(self, s, peak=PQRST):
+    def find_peaks_using_correlation(self, s, peak=PQRST): # todo to fix: problem with long signals because
+        # it is very sensitive to outliers in correlation
         correlation = np.correlate(s, peak) # todo test ss.correlate
         normalized_correlation = correlation / max(correlation)
         peaks_indices, _ = ss.find_peaks(normalized_correlation, height=0.5, distance=66)
@@ -184,27 +184,6 @@ def read_from_file(filename):
         lines = [float(line.rstrip()) for line in f.readlines()]
     return np.array(lines)
 
-
-def plot(s, Fs):
-    #t = np.arange(0, len(s)/Fs, 1/Fs)
-    x = np.arange(0,len(s))
-    plt.plot(x, s)
-    plt.grid('on')
-
-
-# noinspection PyShadowingNames
-def filter_signal_offline(s, Fs):
-    [b, a] = ss.butter(N=2, Wn=0.5, btype='highpass', fs=Fs)
-    # plot_filter_characteristics(b, a, Fs)
-    s = ss.lfilter(b, a, s)
-    [b, a] = ss.butter(N=5, Wn=(49, 51), btype='bandstop', fs=Fs)
-    # plot_filter_characteristics(b, a, Fs)
-    s = ss.lfilter(b, a, s)
-    [b, a] = ss.butter(N=10, Wn=40, btype='lowpass', fs=Fs)
-    # plot_filter_characteristics(b, a, Fs)
-    s = ss.lfilter(b, a, s)
-    return s
-
 def plot_filter_characteristics(b, a, Fs):
     W, h = ss.freqz(b, a)
     f = W * Fs / (2 * np.pi)
@@ -224,6 +203,14 @@ def plot_filter_characteristics(b, a, Fs):
     plt.ylabel('Faza') # Phase Faza
     plt.tight_layout()
 
+def plot_all_filters_characteristics(sp):
+    [b, a] = sp.ba[FilterType.highpass]
+    plot_filter_characteristics(b, a, sp.Fs)
+    [b, a] = sp.ba[FilterType.bandstop]
+    plot_filter_characteristics(b, a, sp.Fs)
+    [b, a] = sp.ba[FilterType.lowpass]
+    plot_filter_characteristics(b, a, sp.Fs)
+    plt.show()
 
 if __name__ == '__main__':
     # filename = r'..\data\2023-02-01_120409.txt' # BAD
@@ -231,7 +218,7 @@ if __name__ == '__main__':
     # filename = r'..\data\2023-02-01_121032.txt' # OK
     # filename = r'..\data\2023-02-01_153643.txt' # OK
     # filename = r'..\data\2023-02-01_153853.txt' # OK
-    filename = r'..\data\2023-02-01_155235.txt' # OK
+    # filename = r'..\data\2023-02-01_155235.txt' # OK
     # filename = r'..\data\2023-02-01_155325.txt' # MEH
     # filename = r'..\data\2023-02-01_155402.txt' # BAD
     # filename = r'..\data\2023-02-01_155958.txt' # MEH - MY ECG
@@ -241,62 +228,49 @@ if __name__ == '__main__':
     # filename = r'..\data\2023-02-04_194943.txt'
     # filename = r'..\data\2023-02-05_034912.txt'
     # filename = r'..\data\2023-02-05_035049.txt'
-
+    filename = r'..\data\2023-02-11_050300_Zuzanna_10min.txt'
 
     s = read_from_file(filename)
     Fs = 200
+    sp = SignalProcessor(Fs)
+    # plot_all_filters_characteristics(sp)
 
-    working_data, measures = hp.process(s, Fs, report_time=True)
-    # print(measures['bpm'])  # returns BPM value
-    # print(measures['rmssd'])  # returns RMSSD HRV measure
-    for x in measures.items():
-        print(x)
+    s = s[360 * Fs:400 * Fs]
 
-    print(s)
-    s_filtered = filter_signal_offline(s, Fs)
+    s_filtered = []
+    for x in s:
+        s_filtered.append(sp.use_all_filters(x))
+    s_filtered = np.array(s_filtered)
 
-    # plt.figure()
-    # plt.plot(np.fft.rfftfreq(len(s_filtered), 1./Fs), abs(np.fft.rfft(s_filtered))**2)
-    # plt.show()
+    peaks_indices = sp.find_peaks_using_correlation(s_filtered)
+    measures = sp.make_ecg_analysis_using_heartpy_on_peaks_found_using_correlation(s_filtered)
+    for measure, value in measures.items():
+        print(f'{measure}: {value}')
 
-    peaks_indices, _ = ss.find_peaks(s_filtered, height=400, distance=66)
-    distances = np.diff(peaks_indices)
-    average_distance = np.mean(distances)
-    print('HR: {:.0f}'.format(Fs*NUMBER_OF_SEC_IN_ONE_MIN/average_distance))
+    # Plots
+    x = np.arange(0, len(s))
 
     plt.figure()
     plt.subplot(2, 1, 1)
-    plot(s, Fs)
+    plt.plot(x/Fs, convert_units_to_volts(s)*1000)
+    plt.grid('on')
+    plt.ylabel('Amplituda [mV]')
     plt.subplot(2, 1, 2)
-    plot(s_filtered, Fs)
-    plt.plot(peaks_indices, s_filtered[peaks_indices], 'x')
+    plt.plot(x/Fs, convert_units_to_volts(s_filtered)*1000)
+    plt.grid('on')
+    plt.xlabel('Czas [s]')
+    plt.ylabel('Amplituda [mV]')
+    plt.tight_layout()
+
+    plt.figure()
+    plt.plot(x/Fs, convert_units_to_volts(s_filtered)*1000)
+    plt.plot(peaks_indices/Fs, convert_units_to_volts(s_filtered[peaks_indices])*1000, 'x')
+    plt.grid('on')
+    plt.xlabel('Czas [s]')
+    plt.ylabel('Amplituda [mV]')
+    plt.tight_layout()
+
     plt.show()
 
-    # Test to check if filtering online and offline works in the same way
-    # sp = SignalProcessor(Fs)
-    # s_filtered2 = []
-    # for x in s:
-    #     s_filtered2.append(sp.use_all_filters(x))
-    # s_filtered2 = np.array(s_filtered2)
-    #
-    # plt.figure()
-    # plt.subplot(3, 1, 1)
-    # plot(s, Fs)
-    # plt.subplot(3, 1, 2)
-    # plot(s_filtered, Fs)
-    # plt.subplot(3, 1, 3)
-    # plot(s_filtered2, Fs)
-    # plt.show()
-
-    # print(all(s_filtered[8400:] == s_filtered2[8400:]))
-    # print(type(s_filtered))
-    # print(type(s_filtered2))
-    # print(len(s_filtered))
-    # print(len(s_filtered2))
-    # print(s_filtered[8400:])
-    # print(s_filtered2[8400:])
-    # for i, x in enumerate(s_filtered[8400:]):
-    #     if x != s_filtered2[8400:][i]:
-    #         print(x, s_filtered2[8400:][i])
 
 
